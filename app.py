305ce083f0f59e9b47247cc89b0cfcfc1a0dfb53
orenouserNamehaKawamura1 <113155719+orenouserNamehaKawamura1@users.noreply.github.com>
2023-09-04 10:18:46 +0900

    利用者と管理者のフォルダ作成

--- app.py
+++ app.py
@@ -6,38 +6,3 @@ app = Flask(__name__)
 @app.route('')
 def index():
     return render_template('index.html')
-
-#アカウント登録
-@app.route('/register')
-def register():
-    return render_template('register.html')
-
-#画像生成画面
-@app.route('/generate')
-def generate():
-    return render_template('generate.html')
-
-#画像表示画面
-@app.route('/preview')
-def preview():
-    return render_template('preview.html')
-
-#利用者メニュー
-@app.route('/user')
-def user_menu():
-    return render_template('user_menu.html')
-
-#画像一覧
-@app.route('/list')
-def img_list():
-    return render_template('list.html')
-
-#画像詳細
-@app.route('/detail')
-def img_detail():
-    return render_template('detail.html')
-
-#画像削除確認画面
-@app.route('/delete')
-def img_delete():
-    return render_template('delete.html')
