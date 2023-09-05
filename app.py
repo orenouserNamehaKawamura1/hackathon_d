403f9d725682433ccdf25d7273b47d9e37f6eaae
菊池響 <h.kikuchi.sys22@morijyobi.ac.jp>
2023-09-05 14:20:23 +0900

    ユーザーログイン #5

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
+import os
 from flask import Flask , render_template, request, url_for
 import db
+from user.account.account import user_bp
+from user.account.login import login_bp
+from admin.adminuser.adminlogin import admin_bp
+import string, random
 
 app = Flask(__name__)
-
+app.secret_key = ''.join(random.choices(string.ascii_letters, k=256))
+app.register_blueprint(user_bp)
+app.register_blueprint(login_bp)
+app.register_blueprint(admin_bp)
 #ログイン画面
-@app.route('')
+@app.route('/')
 def index():
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- db/db.py
+++ db/db.py
@@ -1,6 +1,8 @@
 import os, hashlib, psycopg2, string, random
 
-def get_connection():
-    url = os.environ['_DATABASE_URL']
-    connection = psycopg2.connect(url)
-    return connection
+class DB:
+    
+    def get_connection():
+        url = os.environ['DATABASE_URL']
+        connection = psycopg2.connect(url)
+        return connection
