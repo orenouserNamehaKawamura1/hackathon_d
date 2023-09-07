b3504f4b843bbc5881a8541f839bb3252b6299f4
orenouserNamehaKawamura1 <113155719+orenouserNamehaKawamura1@users.noreply.github.com>
2023-09-07 14:33:42 +0900

    ブループリントの登録

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import db
 from user.account.account import user_bp
 from user.account.login import login_bp
 from admin.adminuser.adminlogin import admin_bp
+from admin.account_list.list import list_bp
 import string, random
 
 
@@ -12,6 +13,7 @@ app.secret_key = ''.join(random.choices(string.ascii_letters, k=256))
 app.register_blueprint(user_bp)
 app.register_blueprint(login_bp)
 app.register_blueprint(admin_bp)
+app.register_blueprint(list_bp)
 
 @app.route('/')
 def index():
