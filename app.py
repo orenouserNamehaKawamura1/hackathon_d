3be9037120c5af488636748a30888b13b94d8b40
orenouserNamehaKawamura1 <113155719+orenouserNamehaKawamura1@users.noreply.github.com>
2023-09-07 10:18:32 +0900

    アカウント一覧と削除、ページネーションの作成

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask , render_template
+import string,random
+from admin.list import list_bp
 
 app = Flask(__name__)
+app.secret_key = ''.join(random.choices(string.ascii_letters, k=256))
+app.register_blueprint(list_bp)
 
-#ログイン画面
-@app.route('')
+@app.route('/')
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- db/db.py
+++
@@ -1,6 +0,0 @@
-import os, hashlib, psycopg2, string, random
-
-def get_connection():
-    url = os.environ['_DATABASE_URL']
-    connection = psycopg2.connect(url)
-    return connection
