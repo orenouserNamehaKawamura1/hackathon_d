6a76c12f9ea3cdaada7649a2110f6d239dd765c0
masa002 <m.obara.sys21@morijyobi.ac.jp>
2023-09-11 10:26:46 +0900

    [fix] コードスタイルの修正

--- db/account_db.py
+++ db/account_db.py
@@ -1,4 +1,8 @@
-import os, psycopg2, string, random, hashlib
+import os
+import psycopg2
+import string
+import random
+import hashlib
 from .db import DB
 
 
@@ -7,23 +11,28 @@ def get_salt():
     salt = ''.join(random.choices(charset, k=30))
     return salt
 
+
 def get_hash(pas, salt):
     b_pw = bytes(pas, "utf-8")
     b_salt = bytes(salt, "utf-8")
     hashed_password = hashlib.pbkdf2_hmac("sha256", b_pw, b_salt, 1000).hex()
     return hashed_password
 
-def inset_ad_user(mail,pas):
-    sql = 'INSERT INTO users VALUES (default, %s, %s, %s)' 
+
+def inset_ad_user(mail, pas):
+    sql = 'INSERT INTO users VALUES (default, %s, %s, %s)'
     salt = get_salt()
     hashed_password = get_hash(pas, salt)
-    try :
-        
+    try:
+
         connection = DB.get_connection()
         cursor = connection.cursor()
-        cursor.execute(sql,(mail,hashed_password,salt))
-        count = cursor.rowcount 
+        cursor.execute(sql, (mail, hashed_password, salt))
+        count = cursor.rowcount
         connection.commit()
+    except Exception as e:
+        print(e)
+        count = 0
     except psycopg2.DatabaseError:
         count = 0
     finally:
@@ -31,98 +40,105 @@ def inset_ad_user(mail,pas):
         connection.close()
     return count
 
-def login(mail,pas):
+
+def login(mail, pas):
     sql = "SELECT pass, salt FROM users WHERE mail = %s AND delete_flag = 'f'"
-    flg = False 
-    try :
+    flg = False
+    try:
         connection = DB.get_connection()
         cursor = connection.cursor()
-        cursor.execute(sql,(mail,))
+        cursor.execute(sql, (mail,))
         mail = cursor.fetchone()
-        if mail != None:
+        if mail is not None:
             salt = mail[1]
-            
+
             hashed_password = get_hash(pas, salt)
             if hashed_password == mail[0]:
                 flg = True
-    except psycopg2.DatabaseError :
+    except psycopg2.DatabaseError:
         flg = False
-    finally : 
+    finally:
         cursor.close()
         connection.close()
     return flg
+
 
 def select_userid(mail):
     connection = DB.get_connection()
     cursor = connection.cursor()
     sql = 'SELECT id FROM users WHERE mail = %s'
-    cursor.execute(sql,(mail,))
+    cursor.execute(sql, (mail,))
     id = cursor.fetchone()
     cursor.close()
     connection.close()
     return id
 
-def ad_login(mail,pas):
+
+def ad_login(mail, pas):
     sql = "SELECT pass, salt FROM admin WHERE mail = %s"
-    flg = False 
-    try :
+    flg = False
+    try:
         connection = DB.get_connection()
         cursor = connection.cursor()
-        cursor.execute(sql,(mail,))
+        cursor.execute(sql, (mail,))
         mail = cursor.fetchone()
-        if mail != None:
+        if mail is not None:
             salt = mail[1]
-            
+
             hashed_password = get_hash(pas, salt)
             if hashed_password == mail[0]:
                 flg = True
-    except psycopg2.DatabaseError :
+    except psycopg2.DatabaseError:
         flg = False
-    finally : 
+    finally:
         cursor.close()
         connection.close()
     return flg
 
-def change_password(mail,pas):
-    sql = "UPDATE users SET pass = %s, salt = %s WHERE mail = %s AND delete_flag = 'f'"
+
+def change_password(mail, pas):
+    sql = '''
+        UPDATE users
+         SET pass = %s, salt = %s
+         WHERE mail = %s AND delete_flag = 'f'
+        '''
     salt = get_salt()
     new_hashed_password = get_hash(pas, salt)
-    
+
     try:
         connection = DB.get_connection()
         cursor = connection.cursor()
-        
+
         cursor.execute(sql, (new_hashed_password, salt, mail))
         connection.commit()
         count = cursor.rowcount
-        
+
     except psycopg2.DatabaseError:
         count = 0
     finally:
         cursor.close()
         connection.close()
-    
+
     return count
 
-def change_ad_password(mail,pas):
+
+def change_ad_password(mail, pas):
     sql = 'UPDATE admin SET pass = %s, salt = %s WHERE mail = %s'
     salt = get_salt()
     new_hashed_password = get_hash(pas, salt)
-    
+
     try:
         connection = DB.get_connection()
         cursor = connection.cursor()
-        
+
         cursor.execute(sql, (new_hashed_password, salt, mail))
         connection.commit()
         count = cursor.rowcount
-        
+
     except psycopg2.DatabaseError:
         count = 0
     finally:
         cursor.close()
         connection.close()
-    
+
     return count
-
-
--- generate.py
+++ generate.py
@@ -2,30 +2,28 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/generate')
 def generate():
     return render_template('generate.html')
 
+
 @app.route('/upload', methods=['POST'])
-def upload_file():    
+def upload_file():
     if 'file' not in request.files:
         return jsonify({'message': 'No file part'})
 
     file = request.files['file']
-    
+
     if file.filename == '':
         return jsonify({'message': 'No selected file'})
 
     # ファイルを保存、または処理するコードをここに追加
     # 例: file.save('uploads/' + secure_filename(file.filename))
-    
-    
-    
-    
-    
-    
+
     return jsonify({'message': 'File uploaded successfully'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
