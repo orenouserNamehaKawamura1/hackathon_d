9970e99c055630bd21d8689081f4076cfc51df69
菊池響 <h.kikuchi.sys22@morijyobi.ac.jp>
2023-09-14 10:06:46 +0900

    [fix]#8 ユーザーメール送信処理の追加

--- user/account/login.py
+++ user/account/login.py
@@ -4,6 +4,9 @@ from db.account_db import login
 from db.account_db import select_userid
 from datetime import timedelta
 from .syntax_check import validate_password
+from .syntax_check import syntax_check
+from .token_generator import generate_token
+from .mail import sendmail_pass
 from db.account_db import change_password
 import string
 import random
@@ -61,7 +64,7 @@ def cahnge_pas():
         return render_template('cahng_pass.html')
 
 
-@login_bp.route('password_cahnge', methods=['POST'])
+@login_bp.route('/password_cahnge', methods=['POST'])
 def password_cahnge():
     mail = request.form.get('mail')
     pas = request.form.get('password')
@@ -70,13 +73,47 @@ def password_cahnge():
         err = "パスワードの形式が間違っています"
         return redirect(url_for('login.cahnge_pas', err=err))
 
-    count = change_password(mail, pas)
-    if count == 1:
-        msg = 'パスワード変更が完了しました'
-        return redirect(url_for('login.index', msg=msg))
+    token = generate_token()
+    session['token'] = token
+    print(session['token'])
+    session['mail'] = mail
+    session['pas'] = pas
+    session.permanent = True
+    login_bp.permanent_session_lifetime = timedelta(minutes=5)
+
+    sendmail_pass(mail, token)
+    return render_template('stand.html')
+
+@login_bp.route('/pass_change/<token>',methods=['GET'])
+def pass_change(token):
+    err = None
+    msg = None
+    if not token:
+        err = "メールを登録して完了させてください"
     else:
-        err = 'アカウントが見つかりません'
+        TOKEN = session.get('token')
+        print(f'TOKEN: {TOKEN} : token: {token}')
+        if TOKEN != token:
+            err = "トークンが無効です"
+        else:
+            mail = session.get('mail')
+            pas = session.get('pas')
+
+            if syntax_check(mail):
+                count = change_password(mail, pas)
+                if count == 1:
+                    msg = 'パスワード変更が完了しました'
+                else:
+                    err = 'パスワード変更に失敗しました'
+            else:
+                err = 'メールアドレスが有効ではありません'
+
+    session.pop('token', None)
+    if err:
         return redirect(url_for('login.cahnge_pas', err=err))
+    elif msg:
+        return redirect(url_for('login.index',msg = msg))
+
 
 
 @login_bp.route('/logout')
